eff9b37a003d4e571f20773098500ebd6d19eff1
luisferlie <luisferlie@gmail.com>
2024-02-12 10:20:33 +0100

    añadida funcionalidad winner

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
-import "./App.css";
-import Player from "./Player/Player";
-import { useState, useEffect } from "react";
+import './App.css'
+import { useState, useEffect } from 'react'
+import Player from './Player/Player'
+
+// 1. definir variables de estado usando useState (activePlayer, score, current, diceNumber)
+// 2. definir funciones para manejar los eventos de click (handleNewGame, handleRollDice, handleHold)
+// 3. pasar las variables de estado y las funciones a los componentes Player y Dice
+// 4. manejar los eventos de click en los botones de New game, Roll dice y Hold
+// 5. manejar el cambio de imagen de dado cuando se hace click en el botón rolldice
+// 5. manejar el cambio de jugador activo cuando se hace click en el botón Hold
+// 6. manejar el cambio de jugador activo cuando se obtiene un 1 al hacer click en el botón Roll dice
+// 7. manejar el cambio de jugador activo cuando se obtiene un 6 al hacer click en el botón Roll dice
+// 8. manejar el cambio de jugador activo cuando se obtiene un número diferente de 1 o 6 al hacer click en el botón Roll dice
+// 9. manejar el cambio de jugador activo cuando se hace click en el botón New game
 
 function App() {
-  // 1. definir variables de estado usando useState (activePlayer, socre, current, diceNumber)
-  // 2. definir funciones para manejar los eventos de click (handleNewGame, handleRollDice, handleHold)
-  // 3. pasar las variables de estado y las funciones a los componentes Player y Dice
-  // 4. manejar los eventos de click en los botones de New game, Roll dice y Hold
-  // 5. manejar el cambio de imagen de dado cuando se hace click en el botón rolldice
-  // 5. manejar el cambio de jugador activo cuando se hace click en el botón Hold
-  // 6. manejar el cambio de jugador activo cuando se obtiene un 1 al hacer click en el botón Roll dice
-  // 7. manejar el cambio de jugador activo cuando se obtiene un 6 al hacer click en el botón Roll dice
-  // 8. manejar el cambio de jugador activo cuando se obtiene un número diferente de 1 o 6 al hacer click en el botón Roll dice
-  // 9. manejar el cambio de jugador activo cuando se hace click en el botón New game
-
-  const [activePlayer, setActivePlayer] = useState(2);
-  const [score, setScore] = useState([0, 0]);
-  const [current, setCurrent] = useState(1);
-  const [diceNumber, setDiceNumber] = useState(3);
+  const [activePlayer, setActivePlayer] = useState(1)
+  const [score, setScore] = useState([0, 0])
+  const [current, setCurrent] = useState(0)
+  const [diceNumber, setDiceNumber] = useState(0)
 
   const handleHold = () => {
-    const newScore = [...score];
-    newScore[activePlayer - 1] += current;
-    setScore(newScore);
-    setActivePlayer(activePlayer === 1 ? 2 : 1);
-    setCurrent(0);
-  };
-  const handleNewGame = (e) => {
-    setActivePlayer(1);
-    setCurrent(0);
-    setScore([0, 0]);
-    setDiceNumber(0);
-  };
+    // para cambiar el score, se debe definir una nueva variable
+    // no modificamos el array, creamos uno nuevo!!!!
+    const newScore = [...score]
+    // newScore[activaPlayer -1] = newScore[activePlayer -1] + current
+    newScore[activePlayer - 1] += current
+    setScore(newScore)
+    setActivePlayer(activePlayer === 1 ? 2 : 1)
+    setCurrent(0)
+  }
+  const handleNewGame = () => {
+    setActivePlayer(1)
+    setScore([0, 0])
+    setCurrent(0)
+    setDiceNumber(0)
+  }
 
-  const handleRollDice = (e) => {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    setDiceNumber(randomNumber);
-  };
+  const handleRollDice = () => {
+    // const randomNumber = Math.floor(Math.random() * 6) + 1
+    // setDiceNumber(randomNumber)
+    setDiceNumber(Math.floor(Math.random() * 6) + 1)
+  }
 
   useEffect(() => {
     if (diceNumber === 1) {
-      setActivePlayer((activePlayer) => (activePlayer === 1 ? 2 : 1));
+      setActivePlayer((activePlayer) => (activePlayer === 1 ? 2 : 1))
+      setCurrent(0)
     } else {
-      setCurrent((current) => current + diceNumber);
+      // setCurrent (current + diceNumber)
+      setCurrent((current) => current + diceNumber)
     }
-  }, [diceNumber]);
+  }, [diceNumber])
 
   return (
     <main>
       <Player
         name="Player 1"
-       
         score={score[0]}
         current={activePlayer === 1 && current}
         isActive={activePlayer === 1}
@@ -61,20 +66,24 @@ function App() {
         current={activePlayer === 2 && current}
         isActive={activePlayer === 2}
       />
-      <img src={`dice-${diceNumber}.png`} alt="Playing dice" className="dice" />
-
+      {diceNumber && (
+        <img
+          src={`dice-${diceNumber}.png`}
+          alt="Playing dice"
+          className="dice"
+        />
+      )}
       <button className="btn btn--new" onClick={handleNewGame}>
         🔄 New game
       </button>
       <button className="btn btn--roll" onClick={handleRollDice}>
-        {" "}
         🎲 Roll dice
       </button>
       <button className="btn btn--hold" onClick={handleHold}>
         📥 Hold
       </button>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -2,22 +2,37 @@ import './Player.css'
 // props = {name: 'Player 1', score: 43, current: 0, isActive: true}
 function Player({ name, score, current, isActive }) {
   // const { name, score, current, isActive } = props
-  let classPlayer=
-  isActive?
-    score>100?
-      'player--winner':
-      'player player--active'
-  :'player'
-  
+
+  // let classPlayer
+  // if (isActive) {
+  //   if (score >= 100) {
+  //     classPlayer = 'player player--winner'
+  //   } else {
+  //     classPlayer = 'player player--active'
+  //   }
+  // } else {
+  //   classPlayer = 'player'
+  // }
+
+  const classPlayer =
+    score > 10
+      ? 'player player--winner'
+      : isActive
+      ? 'player player--active'
+      : 'player'
+
+  console.log(
+    `Renderizando el componente Player para ${name} con 
+    la clase ${classPlayer} y un score de ${score}`
+  )
 
   return (
-
     <section className={classPlayer}>
       <h2 className="name">{name}</h2>
       <p className="score">{score}</p>
       <div className="current">
         <p className="current-label">Current</p>
-        <p className="current-score">{current||0}</p>
+        <p className="current-score">{current || 0}</p>
       </div>
     </section>
   )
